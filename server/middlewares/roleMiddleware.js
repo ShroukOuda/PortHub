324801e83a8358a967099385bf24ca67fd3c6bcf
ShroukOuda <s36568961@gmail.com>
2025-07-28 08:33:15 +0300

    ad role middleware

---
+++ server/middlewares/roleMiddleware.js
@@ -0,0 +1,16 @@
+const roleMiddleware = (roles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized access' });
+        }
+
+        const hasRole = roles.includes(req.user.role); 
+        if (!hasRole) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+
+        next();
+    }
+}
+
+module.exports = roleMiddleware;
