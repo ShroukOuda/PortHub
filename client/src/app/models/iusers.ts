1e21df3719af0b596bf881ac20969469b6b34b65
ShroukOuda <s36568961@gmail.com>
2025-07-30 18:24:37 +0300

    add client side

---
+++ client/src/app/models/iusers.ts
@@ -0,0 +1,11 @@
+export interface Iusers {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    gender: string;
+    age: number;
+    image: string;
+    role: string;
+}
