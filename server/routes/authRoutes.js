242445dfea3c4f4bf5f3d7d027b84e9496ba7992
ShroukOuda <s36568961@gmail.com>
2025-08-17 18:32:07 +0300

    Refactor: move login , register & logout to authRoutes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    registerUser,
+    loginUser,
+    logoutUser
+} = require('../controllers/authController');
+
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', authMiddleware, logoutUser);
+
+module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { 
-    registerUser, 
-    loginUser,
-    logoutUser,
     getAllUsers, 
     getUserById, 
     updateUser, 
@@ -13,15 +10,14 @@ const {
 const authMiddleware = require('../middlewares/authMiddleware');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 
-router.post('/register', registerUser); 
-router.post('/login', loginUser);       
+    
 
 
 router.get('/', getAllUsers);          
 router.get('/:id',  getUserById);      
 router.put('/:id', authMiddleware, roleMiddleware(['admin']), updateUser);       
 router.delete('/:id', authMiddleware, roleMiddleware(['admin']), deleteUser);   
-router.post('/logout', authMiddleware, logoutUser);    
+ 
 
 
 module.exports = router;
