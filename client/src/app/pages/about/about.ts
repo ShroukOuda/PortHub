6b0bb13e7c89d77a783e9235cd971e72dc57bcc1
ShroukOuda <s36568961@gmail.com>
2025-08-17 18:41:05 +0300

    update about page

--- client/src/app/pages/about/about.ts
+++ client/src/app/pages/about/about.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+  faArrowRight = faArrowRight;
+  faHeart = faHeart;
 
 }
