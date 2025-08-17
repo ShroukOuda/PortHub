2d3428d4ab5a5faac99f070c094d634abd09cd41
ShroukOuda <s36568961@gmail.com>
2025-08-17 18:33:55 +0300

    add setTimeout

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env.test' });
 
+jest.setTimeout(20000);
+
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGODB_URI_TEST);
 });
