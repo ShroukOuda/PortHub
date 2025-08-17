e4918f509f078e496ee2b4efd297677ae4566307
ShroukOuda <s36568961@gmail.com>
2025-08-17 18:38:39 +0300

    add login, signup routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Portfolios } from './pages/portfolios/portfolios';
 import { Contact } from './pages/contact/contact';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'about', component: About},
     {path: 'portfolios', component: Portfolios},
-    {path: 'contact', component: Contact}
+    {path: 'contact', component: Contact},
+    {path: 'login', component: Login},
+    {path: 'signup', component: Signup}
 ];
