1a4c1924f159f3a8f1dead6149cb19cc77514945
ShroukOuda <s36568961@gmail.com>
2025-08-18 20:19:49 +0300

    add portfolio-view

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { Portfolios } from './pages/portfolios/portfolios';
 import { Contact } from './pages/contact/contact';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
+import { PortfolioView } from './features/portfolio-viewer/pages/portfolio-view/portfolio-view';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'about', component: About},
     {path: 'portfolios', component: Portfolios},
+    {path: 'portfolio-view/:userId', component: PortfolioView},
     {path: 'contact', component: Contact},
     {path: 'login', component: Login},
     {path: 'signup', component: Signup}
