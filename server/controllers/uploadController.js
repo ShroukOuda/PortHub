dd07dcc01afad7ad6bc7a89e3e685aded804ff63
ShroukOuda <s36568961@gmail.com>
2025-08-18 16:33:19 +0300

    add upload controller

---
+++ server/controllers/uploadController.js
@@ -0,0 +1,6 @@
+const uploadFile = (req, res) => {
+  if (!req.file) return res.status(400).json({ message: 'No file uploaded' });
+  res.status(200).json({ path: req.file.path });
+};
+
+module.exports = { uploadFile };
